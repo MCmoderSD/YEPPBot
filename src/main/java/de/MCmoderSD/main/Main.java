194b00456770963526f2982fa1740159a7143a20
mcmodersd <mail@mcmodersd.de>
2024-12-19 11:48:50 +0100

    Upgrade to Twitch4J 1.23.0 Fixed HTTPS Server for Linux improved docker file

--- src/main/java/de/MCmoderSD/main/Main.java
+++ src/main/java/de/MCmoderSD/main/Main.java
@@ -18,7 +18,7 @@ import static de.MCmoderSD.utilities.other.Format.*;
 public class Main {
 
     // Constants
-    public static final String VERSION = "1.22.1";
+    public static final String VERSION = "1.23.0";
 
     // Bot Config
     public static final String BOT_CONFIG = "/config/BotConfig.json";
