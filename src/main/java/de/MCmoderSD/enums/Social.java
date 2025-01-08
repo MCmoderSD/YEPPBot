ee2879f09ac3f305935651a8cd7f046ddd54b0fd
mcmodersd <mail@mcmodersd.de>
2025-01-08 16:54:57 +0100

    Social & Rank WIP

---
+++ src/main/java/de/MCmoderSD/enums/Social.java
@@ -0,0 +1,32 @@
+package de.MCmoderSD.enums;
+
+public enum Social {
+
+    // Constants
+    INSTAGRAM("instagram", "insta"),
+    TWITTER("twitter", "x"),
+    YOUTUBE("youtube", "yt");
+
+    // Attributes
+    private final String name;
+    private final String[] aliases;
+
+    // Constructor
+    Social(String name, String... aliases) {
+        this.name = name;
+        this.aliases = aliases;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public static Social getSocial(String name) {
+        for (Social social : values()) {
+            if (social.getName().equalsIgnoreCase(name)) return social;
+            for (String alias : social.aliases) if (alias.equalsIgnoreCase(name)) return social;
+        }
+        return null;
+    }
+}
