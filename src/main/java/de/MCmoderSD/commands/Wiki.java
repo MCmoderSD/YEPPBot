d8a0bcbae29974356ae8508054373d9f5dd0f6f4
mcmodersd <mail@mcmodersd.de>
2025-01-22 11:27:28 +0100

    Wiki command Rework and Dependency cleanup

--- src/main/java/de/MCmoderSD/commands/Wiki.java
+++ src/main/java/de/MCmoderSD/commands/Wiki.java
@@ -5,22 +5,16 @@ import de.MCmoderSD.core.BotClient;
 import de.MCmoderSD.core.MessageHandler;
 import de.MCmoderSD.objects.TwitchMessageEvent;
 import de.MCmoderSD.OpenAI.modules.Chat;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import de.MCmoderSD.wikipedia.core.WikipediaAPI;
+import de.MCmoderSD.wikipedia.data.Page;
+import de.MCmoderSD.wikipedia.enums.Language;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import static de.MCmoderSD.utilities.other.Format.*;
 
 public class Wiki {
-
-    // Constants
-    private final String errorRetrievingWeatherData;
-    private final String noSummaryFoundForThisTopic;
 
     // Constructor
     public Wiki(BotClient botClient, MessageHandler messageHandler, Chat chat) {
@@ -33,8 +27,11 @@ public class Wiki {
         String description = "Sucht auf Wikipedia nach einem Thema und gibt eine Zusammenfassung zurück. " + syntax;
 
         // Constants
-        errorRetrievingWeatherData = "Fehler beim Abrufen der Wikipedia-Zusammenfassung:";
-        noSummaryFoundForThisTopic = "Keine Zusammenfassung für dieses Thema gefunden.";
+        String errorFetchingWikipedia = "Fehler beim Abrufen der Wikipedia-Zusammenfassung:";
+        String noSummaryFoundForThisTopic = "Keine Zusammenfassung für dieses Thema gefunden.";
+
+        // Initialize Wikipedia
+        WikipediaAPI wiki = new WikipediaAPI();
 
         // Register command
         messageHandler.addCommand(new Command(description, name) {
@@ -53,23 +50,35 @@ public class Wiki {
                 // Check for topic
                 if (args.isEmpty()) response = syntax;
                 else {
-
-                    // Query Wikipedia
-                    String topic = processArgs(args);
                     try {
+                        // Check for Language
+                        Language language = Language.ENGLISH.getLanguage(args.getFirst());
+                        if (language == null) language = Language.GERMAN;
+                        else args.removeFirst(); // Remove Language
 
-                        // Get Wikipedia summary
-                        String summary = trimMessage(getWikipediaSummary(topic)); // Get Wikipedia summary
+                        // Query Wikipedia
+                        String topic = processArgs(args);
 
-                        // Check if summary is too long
+                        // Wikipedia API
+                        Page wikiPage;
+                        try {
+                            wikiPage = wiki.query(language, topic);
+                        } catch (IOException | InterruptedException e) {
+                            botClient.respond(event, getCommand(), noSummaryFoundForThisTopic);
+                            return;
+                        }
+
+                        // Extract Summary and Check Length
+                        String summary = wikiPage.getExtract();
                         if (summary.length() <= 500) response = summary;
-                        else response = trimMessage(chat.prompt(null, "Please summarize the following text using the original language used in the text. Answer only in 500 or less chars", summary, 0d, null, null, null, null));
+                        else
+                            response = trimMessage(chat.prompt(null, "Please summarize the following text into " + language.getName() + " used in the text. Answer only in 500 or less chars", summary, 0d, null, null, null, null));
 
                         // Filter Response for argument injection
                         response = removePrefix(response);
 
-                    } catch (IOException e) {
-                        response = trimMessage(String.format("%s %s", errorRetrievingWeatherData, e.getMessage()));
+                    } catch (Exception e) {
+                        response = errorFetchingWikipedia;
                     }
                 }
 
@@ -77,17 +86,5 @@ public class Wiki {
                 botClient.respond(event, getCommand(), response);
             }
         });
-    }
-
-    // Get Wikipedia summary
-    private String getWikipediaSummary(String topic) throws IOException {
-        String apiUrl = String.format("https://de.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&explaintext=true&redirects=true&titles=%s", URLEncoder.encode(topic, StandardCharsets.UTF_8));
-        Document doc = Jsoup.connect(apiUrl).ignoreContentType(true).get();
-        JSONObject json = new JSONObject(doc.text());
-        JSONObject pages = json.getJSONObject("query").getJSONObject("pages");
-        String firstPageKey = pages.keys().next();
-        JSONObject page = pages.getJSONObject(firstPageKey);
-        if (page.has("extract")) return page.getString("extract");
-        else throw new IOException(noSummaryFoundForThisTopic);
     }
 }
