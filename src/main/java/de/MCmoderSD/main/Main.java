eec350d97238e74943f3c30c82efca8f42a4306b
mcmodersd <mail@mcmodersd.de>
2025-11-05 20:11:59 +0100

    Updated Version and gitignore

--- src/main/java/de/MCmoderSD/main/Main.java
+++ src/main/java/de/MCmoderSD/main/Main.java
@@ -19,7 +19,7 @@ import java.util.stream.Stream;
 public class Main {
 
     // Constants
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.25.0-PTB-1";
 
     // Flags
     public static boolean DEBUG = false;
