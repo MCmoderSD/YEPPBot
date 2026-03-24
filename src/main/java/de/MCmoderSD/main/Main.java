6c008eebf462f96eec9b81c90bf4a903cf4c7444
MCmoderSD <mail@mcmodersd.de>
2026-03-24 04:26:53 +0100

    Release 1.26.0 - Version Update - Updated application version to 1.26.0-PTB-1

--- src/main/java/de/MCmoderSD/main/Main.java
+++ src/main/java/de/MCmoderSD/main/Main.java
@@ -28,7 +28,7 @@ import static de.MCmoderSD.utilities.MessageHelper.ICON;
 public class Main {
 
     // Constants
-    public static final String VERSION = "1.25.0-PTB-1";
+    public static final String VERSION = "1.26.0-PTB-1";
 
     // Flags
     public static boolean DEBUG = false;
